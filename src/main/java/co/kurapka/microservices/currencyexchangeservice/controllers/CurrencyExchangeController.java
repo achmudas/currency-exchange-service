cf9fc0e94ae4c8c928d369536698e981af15be74
Remigijus <achmudass@gmail.com>
2019-11-17 17:40:21 +0200

    Introducing Sleuth

--- src/main/java/co/kurapka/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
+++ src/main/java/co/kurapka/microservices/currencyexchangeservice/controllers/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package co.kurapka.microservices.currencyexchangeservice.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,17 +13,20 @@ import co.kurapka.microservices.currencyexchangeservice.model.ExchangeValueJpaRe
 
 @RestController
 public class CurrencyExchangeController {
-	
+
+	private static final Logger log = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private ExchangeValueJpaRepository jpaRepository;
-	
+
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		ExchangeValue exValue = jpaRepository.findByFromAndTo(from, to);
 		exValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+		log.info("exValue -> {}", exValue);
 		return exValue;
 	}
 
